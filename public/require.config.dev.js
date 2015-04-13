79feeb2822bc7a4fb8754683999cbcf5f02eede5
Jamon Terrell <jamon@terrell.cc>
2015-04-13 14:57:13 -0500

    added missing require.js configs

---
+++ public/require.config.dev.js
@@ -0,0 +1,10 @@
+require.config({
+    paths: {
+        "react": "/static/portal/0.0/lib/react-with-addons",
+        "example": "/static/example/0.0/index-react"
+    }
+});
+require(['example', 'react'], function(Example, React) {
+    console.log(Example);
+    React.render(React.createElement(Example.Test, null), document.body);
+});
