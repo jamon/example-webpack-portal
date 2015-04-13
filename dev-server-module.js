9f348ea74893f76a036a2582016462be4d37b7a1
Jamon Terrell <jamon@terrell.cc>
2015-04-13 14:52:06 -0500

    many changes, re-organized

--- dev-server-module.js
+++ dev-server-module.js
@@ -1,8 +1,21 @@
 var webpack = require('webpack');
+var WebpackDevMiddleware = require('webpack-dev-middleware');
+var serveStatic = require('serve-static');
 var conf = JSON.parse(require('fs').readFileSync(__dirname + '/package.json'));
+var httpProxy = require('http-proxy').createProxyServer();
+
+var rewriteUrl = function(path, replacePath) {
+    return function(req, res, next) {
+        var queryIndex = req.url.indexOf('?');
+        var query = queryIndex >= 0 ? req.url.substr(queryIndex) : "";
+        req.url = req.path.replace(path, replacePath) + query;
+        console.log("rewriting ", req.originalUrl, req.url);
+        next();
+    };
+};
 
 module.exports = {
-    load: function(app, shared, options) {
+    load: function(app, options) {
         function registerStatic() {
             var regex = [
                 options.static,
@@ -13,20 +26,10 @@ module.exports = {
             var source = new RegExp(regex);
             var target = __dirname + "/public";
             console.log("static", source, "=>", target);
-            app.get(source, shared.rewriteUrl(source, "/$1"), shared.express.static(target));
+            app.get(source, rewriteUrl(source, "/$1"), serveStatic(target, {'index': ['index.html']}));
+            //app.get(source, rewriteUrl(source, "/$1"), shared.express.static(target));
         }
-        function registerApi() {
-            var proxy = conf.devServer.proxy;
-            var source = new RegExp(proxy.source);
-            var proxyOptions = {target: proxy.target, ws: true};
-            console.log("api", source, "=>", proxy.target);
-            app.all(source, shared.rewriteUrl(source, proxy.rewrite), function(req, res, next) {
-                shared.proxy.web(req, res, proxyOptions, function(err) {
-                    console.error("cannot proxy to " + proxyOptions.target + " (" + err.message + ")");
-                });
-            });
 
-        }
         function registerWebpack() {
             var path = [
                 options.static,
@@ -42,13 +45,10 @@ module.exports = {
                 publicPath: path,
                 stats: { colors: true}
             };
-            app.use(shared.WebpackDevMiddleware(shared.webpack(webpackConf), webpackDevConf));
-
-
+            app.use(WebpackDevMiddleware(webpack(webpackConf), webpackDevConf));
         }
 
         //registerWebpack();
         registerStatic();
-        //registerApi();
     }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,11 @@
 var webpack = require('webpack');
-var conf = JSON.parse(require('fs').readFileSync('package.json'));
-var rewriteUrl = function(replacePath) {
-    return function(req, opt) {
-        var queryIndex = req.url.indexOf('?');
-        var query = queryIndex >= 0 ? req.url.substr(queryIndex) : "";
-        req.url = req.path.replace(opt.path, replacePath) + query;
-        //console.log("rewriting ", req.originalUrl, req.url);
-    };
-};
 module.exports = {
     context: __dirname + '/src',
-    entry: './index.js', // { main: ['./index.js', 'lib/react-with-addons']},
+    entry: './index.js',
     output: {
-        library: conf.name,
-        filename: conf.name + ".js",
+        filename: "[name].js",
         libraryTarget: "amd",
-        path: __dirname + '/public',
+        path: "/",
         sourceMapFilename: "[file].map"
     },
     module: {
@@ -25,35 +15,11 @@ module.exports = {
         }]
     },
     externals: {
-         'lib/react': "amd ./lib/react-with-addons"
-    },
-    devServer: {
-        contentBase: false, //'./public',
-        proxy: [
-            {
-                path: new RegExp("/api/example/1/(.*)"),
-                rewrite: rewriteUrl("/$1"),
-                target: "http://127.0.0.1:3000/"
-            }
-        ],
-        content: [
-            {
-                path: "*",
-                target: "public"
-            },
-            {
-                path: new RegExp("/test/(.*)"),
-                rewrite: rewriteUrl("/$1"),
-                target: "public"
-            }
-        ],
-        stats: {
-            colors: true
-        }
+         'lib/react': "amd react"
     },
     plugins: [
-        new webpack.OldWatchingPlugin()
-        //new webpack.optimize.UglifyJsPlugin()
+        new webpack.OldWatchingPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
     ],
     debug: true,
     devtool: 'source-map'
