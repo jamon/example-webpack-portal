46d13f962b4bbfbddea08731a15e10d6f3b23a32
jgvogt <jerry.vogt@daugherty.com>
2015-04-17 08:41:43 -0500

    Switched to Account

---
+++ src/account.js
@@ -0,0 +1,9 @@
+// entry point
+define(['lib/react'], function(React) {
+    return React.createClass({
+        displayName: 'example/account',
+        render: function() {
+            return <div>Hello test</div>;
+        }
+    });
+});
